8be3352f18eb7e29990cd2fc0c262be898687605
laisee <laisee@users.noreply.github.com>
2026-01-13 22:01:45 +0800

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
+
